3c0791077a5ea53ab12f8c5f17a5ff84fd914556
ibad nurhamim <ibad.nurhamim.konsultan@work.bri.co.id>
2020-09-14 16:45:17 +0700

    Currency exhange example

---
+++ app/src/main/java/com/tutorial/currencyconverter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.tutorial.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText amountEdt;
+
+    public void convertFunc(View view) {
+        amountEdt = findViewById(R.id.editTextNumber);
+        Double parseAmountToDouble = Double.parseDouble(amountEdt.getText().toString());
+        Double convertToRupiah = parseAmountToDouble * 14952;
+
+        Locale locale = new Locale("idn", "id");
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(locale);
+
+        Toast.makeText(this, "Rp " +  numberFormat.format(convertToRupiah), Toast.LENGTH_LONG).show();
+
+        Log.i("Info Amount", amountEdt.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
